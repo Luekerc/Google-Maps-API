c1f840980d9ee4e262f97ab0124a08e5ddde9762
Charles Lueker <charleslueker@yahoo.com>
2015-05-09 14:16:12 -0500

    went the wrong direction on the zoom

--- scripts/main.js
+++ scripts/main.js
@@ -105,7 +105,7 @@ function initialize(){
                              map = new google.maps.Map(
                                 mapContainer[ 0 ],
                                 {
-                                    zoom: 4,
+                                    zoom: 16,
                                     center: new google.maps.LatLng(
                                         myLat,
                                         myLong

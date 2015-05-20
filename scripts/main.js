f60619f60933d58e6fa4a78de26183411c6dbf20
Charles Lueker <charleslueker@yahoo.com>
2015-05-20 14:36:23 -0500

    adding second map back in

--- scripts/main.js
+++ scripts/main.js
@@ -112,8 +112,49 @@ function initialize(){
         }
 }
 
+
+function searchMap() {
+  var pyrmont = new google.maps.LatLng(30.252743499999998, -97.76361469999999);
+
+  map = new google.maps.Map(document.getElementById('map-canvas2'), {
+    center: pyrmont,
+    zoom: 15
+  });
+
+  var request = {
+    location: pyrmont,
+    radius: 500,
+    types: ['store']
+  };
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
+}
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
+
 $("#button").click(function(){
     initialize();
+    searchMap();
 })
 
 }

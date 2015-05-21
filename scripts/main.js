aca30191158d37e0a0f8e60cbc1c683b54a6b9fa
Charles Lueker <charleslueker@yahoo.com>
2015-05-20 20:03:21 -0500

    getting back to life

--- scripts/main.js
+++ scripts/main.js
@@ -58,47 +58,14 @@ function initialize(){
                         }
                     );
  
+
+
                     myLatlng = new google.maps.LatLng(myLat,myLong);
                     marker = new google.maps.Marker({
                         position: myLatlng,
                         map: map,
                         title: 'You are here'
                     });
-                    //start search
-                    map = new google.maps.Map(document.getElementById('map-canvas2'), {
-                        center: pyrmont,
-                        zoom: 15
-                    });
-
-                    var request = {
-                      location: pyrmont,
-                      radius: 1000,
-                      types: ['pharmacy']
-                    };
-                    infowindow = new google.maps.InfoWindow();
-                    var service = new google.maps.places.PlacesService(map);
-                      service.nearbySearch(request, callback);
-                    }
-
-                    function callback(results, status) {
-                      if (status == google.maps.places.PlacesServiceStatus.OK) {
-                        for (var i = 0; i < results.length; i++) {
-                          createMarker(results[i]);
-                        }
-                      }
-                    }
-
-                    function createMarker(place) {
-                      var placeLoc = place.geometry.location;
-                      var marker = new google.maps.Marker({
-                        map: map,
-                        position: place.geometry.location
-                        });
-
-                      google.maps.event.addListener(marker, 'click', function() {
-                        infowindow.setContent(place.name);
-                        infowindow.open(map, this);
-                      });//end of search function
 
                 },
 

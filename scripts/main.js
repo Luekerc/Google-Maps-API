5c6a6b1e90475b0993e9bd6c6a8334c2e5ce2580
Charles Lueker <charleslueker@yahoo.com>
2015-05-20 19:37:06 -0500

    changed scope again

--- scripts/main.js
+++ scripts/main.js
@@ -123,8 +123,8 @@ function initialize(){
                 },
                 (1000 * 60 * 5)
             );
-        }
-                        function callback(results, status) {
+                //beginning of second half of search
+                    function callback(results, status) {
                       if (status == google.maps.places.PlacesServiceStatus.OK) {
                         for (var i = 0; i < results.length; i++) {
                           createMarker(results[i]);
@@ -143,6 +143,9 @@ function initialize(){
                         infowindow.setContent(place.name);
                         infowindow.open(map, this);
                       });//end of search function
+
+        }
+
 }
 
 //developers.google.com/maps/documentation/javascript/examples/place-search
